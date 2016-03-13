a4cee18b4eae1dd469cceb2eee7d72a45cbfefd3
Bruno Mota <bruno12mota@gmail.com>
2016-03-13 17:34:09 +0100

    root data connect namespace same query with different variables

--- lib/decorators/root-data-connect.jsx
+++ lib/decorators/root-data-connect.jsx
@@ -1,5 +1,7 @@
 import debounce from 'lodash.debounce';
+import forEach from 'lodash.foreach';
 import hoistStatics from 'hoist-non-react-statics';
+import isEqual from 'lodash.isequal';
 import Q from 'q';
 import React, {Component, PropTypes} from 'react';
 import {mergeFragments, buildQueryAndVariables} from 'relax-fragments';
@@ -22,6 +24,7 @@ export default function rootDataConnect () {
         this.bundle = {};
         this.childFetchDataBind = ::this.childFetchData;
         this.fetchDebounce = debounce(::this.fetchData, 10);
+        this.scopeID = 0;
       }
 
       getChildContext () {
@@ -41,11 +44,34 @@ export default function rootDataConnect () {
         // TODO check if same query with different variables (will need to scope varibles)
         // e.g. {somepages: pages (..) {..}, morepages: pages (..) {..}}
 
+        // Check for same query with different variables
+        const scopes = {};
+        if (this.bundle.fragments) {
+          forEach(fragments, (fragment, queryName) => {
+            if (this.bundle.fragments[queryName]) {
+              // Same query name detected, will have to check if variables are the same
+              const sameVariables = isEqual(
+                variables && variables[queryName],
+                this.bundle.variables && this.bundle.variables[queryName]
+              );
+
+              if (!sameVariables) {
+                // Will have to scope it
+                const scope = `relate_${this.scopeID++}`;
+                const newQueryName = `${scope}: ${queryName}`;
+                fragments[newQueryName] = fragments[queryName];
+                scopes[scope] = queryName;
+                delete fragments[queryName];
+              }
+            }
+          });
+        }
+
         this.bundle = {
           fragments: mergeFragments(this.bundle.fragments || {}, fragments || {}),
           variables: Object.assign(this.bundle.variables || {}, variables || {}),
           connectors: Object.assign(this.bundle.connectors || {}, {
-            [ID]: {fragments, mutations}
+            [ID]: {fragments, mutations, scopes}
           })
         };
 
